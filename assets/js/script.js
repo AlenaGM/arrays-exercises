c5d027e8cd0097d058cc53bb4699ed5822a4199a
AlenaGM <alena.guillaume4@gmail.com>
2022-09-16 16:19:00 +0200

    upd eng

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-//Спросить значения, рассортировать по возрастанию, суммировать
+//Ask numbers, arrange in ascending order, sum numbers
 let numbers = [];
 
-function sumInput(){
-
-    while (true){
-    let nbr = prompt(`Введите число`);
+function sumInput() {
+  while (true) {
+    let nbr = prompt(`Enter a number`);
 
     if (nbr === "" || nbr === null || !isFinite(nbr)) break;
 
@@ -13,7 +12,7 @@ function sumInput(){
 
   numbers.sort((a, b) => a - b);
   console.log(numbers);
-  alert ('Массив по возрастанию: ' + numbers);
+  alert("Array in ascending order: " + numbers);
 
   let sumNumbers = 0;
 
@@ -21,44 +20,39 @@ function sumInput(){
     sumNumbers += nbr;
   }
 
-  console.log(sumNumbers);
-  alert ('Сумма значений массива = ' + sumNumbers);
-};
-
-//Вывести на экран 1-й элемент массива
-let files = ["js", "css", "html"];
-
-function firstElem(){
-
-  console.log(files[0]);
-  alert('Первый элемент массива: ' + files[0]);
-};
-
-
-let mess = [0, 1, false, 2, undefined, '', 3, null];
-
-//Убрать лишнее из массива
-function deleteMess(value) {
-  return value != false && value != '' && value != undefined && value != null;
-};
-
-let filtered = [0, 1, false, 2, undefined, '', 3, null].filter(deleteMess);
-
-console.log(filtered);
-
-function showFiltered() {
-
-  alert('Все лишнее убрали, вот: ' + filtered);
-};
-
-//Найти индекс массива, длина которого > 3
-let multinumbers = [[1,2], [1,2,3], [1,2,3,4]];
-
-function showIndex(){
-
-for (let i = 0; i < multinumbers.length; i++) {
-    if (multinumbers[i].length > 3)
-      alert('Индекс массива: ' + i);
-  }
+  alert("Sum of array's elements = " + sumNumbers);
 }
 
+//Display the first element of an array
+let files = ["js", "css", "html"];
+
+function firstElem() {
+  console.log(files[0]);
+  alert("The first element of array: " + files[0]);
+}
+
+let mess = [0, 1, false, 2, undefined, "", 3, null];
+
+//Filter from unwanted values
+function deleteMess(value) {
+  return value != false && value != "" && value != undefined && value != null;
+}
+
+let filtered = [0, 1, false, 2, undefined, "", 3, null].filter(deleteMess);
+
+function showFiltered() {
+  alert("All unwanted values are cleared! Your new array is: " + filtered);
+}
+
+//Find the index of the array whose length is > 3
+let multinumbers = [
+  [1, 2],
+  [1, 2, 3],
+  [1, 2, 3, 4],
+];
+
+function showIndex() {
+  for (let i = 0; i < multinumbers.length; i++) {
+    if (multinumbers[i].length > 3) alert("Array's index: " + i);
+  }
+}
